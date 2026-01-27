9826d3135ff7e018541ca7c65c4170e4fe43d568
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-01-27 20:00:06 -0300

    Create Position class

--- src/application/Aplic.java
+++ src/application/Aplic.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Aplic {
 
-	public static void main(String[] args) {
-		System.out.println("Inicializa Projeto");
+	public static void main(String[] args) {	
+		Position pos = new Position(4,5);
+		System.out.println(pos);
 	}
 
 }
