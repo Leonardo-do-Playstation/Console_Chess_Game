feae9b3a0e04f5c5045f9c34e63003fdeec91714
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-01-27 20:33:43 -0300

    Starts to implement Board em Piece classes

--- src/application/Aplic.java
+++ src/application/Aplic.java
@@ -1,5 +1,6 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Aplic {
@@ -7,6 +8,10 @@ public class Aplic {
 	public static void main(String[] args) {	
 		Position pos = new Position(4,5);
 		System.out.println(pos);
+		
+		Board board = new Board(8, 9);
+		
+		System.out.printf("%d,%d", board.getRows(), board.getColumns());
 	}
 
 }
