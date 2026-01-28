ab378bab5af2aad9b3586dbfeb7206d65c6a15a7
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-01-28 12:23:27 -0300

    Chess layer and print the board

--- src/application/Aplic.java
+++ src/application/Aplic.java
@@ -1,17 +1,13 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Aplic {
 
-	public static void main(String[] args) {	
-		Position pos = new Position(4,5);
-		System.out.println(pos);
+	public static void main(String[] args) {			
+		ChessMatch chessMatch = new ChessMatch();
 		
-		Board board = new Board(8, 9);
-		
-		System.out.printf("%d,%d", board.getRows(), board.getColumns());
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
